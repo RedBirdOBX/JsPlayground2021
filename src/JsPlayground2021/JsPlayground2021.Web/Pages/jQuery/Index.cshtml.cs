91847c2916e240112501a3d06d3d2295045dcbfb
RedBirdOBX <shane.fowlkes.70@gmail.com>
2022-06-29 16:22:02 -0400

    js refactor

---
+++ src/JsPlayground2021/JsPlayground2021.Web/Pages/jQuery/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsPlayground.Pages.Javascript
+{
+    public class JQueryIndex : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
