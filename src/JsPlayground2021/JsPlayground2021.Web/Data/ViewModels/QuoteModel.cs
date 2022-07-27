f85e74a32295cff2bc461b99248ee217b45127e1
RedBirdOBX <shane.fowlkes.70@gmail.com>
2022-07-27 08:11:01 -0400

    wip

---
+++ src/JsPlayground2021/JsPlayground2021.Web/Data/ViewModels/QuoteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JsPlayground2021.Web.Data.ViewModels
+{
+    public class QuoteModel
+    {
+        public string Quote { get; set; }
+        public string Author { get; set; }
+        public string Source { get; set; }
+    }
+}
